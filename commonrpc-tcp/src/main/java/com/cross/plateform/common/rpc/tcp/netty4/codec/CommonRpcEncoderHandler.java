160feae69f7d738ae75d5e20fcd31f41e3147b69
liubing44 <liubing44@wanda.cn>
2015-04-04 14:20:03 +0800

    解决服务端关闭,close 端口异常

--- commonrpc-tcp/src/main/java/com/cross/plateform/common/rpc/tcp/netty4/codec/CommonRpcEncoderHandler.java
+++ commonrpc-tcp/src/main/java/com/cross/plateform/common/rpc/tcp/netty4/codec/CommonRpcEncoderHandler.java
@@ -4,11 +4,8 @@
 package com.cross.plateform.common.rpc.tcp.netty4.codec;
 
 import com.cross.plateform.common.rpc.core.protocol.all.CommonRpcProtocol;
-import com.cross.plateform.common.rpc.tcp.netty4.client.factory.CommonRpcTcpClientFactory;
-
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelPromise;
 import io.netty.handler.codec.MessageToByteEncoder;
 /**
  * @author liubing1
@@ -25,11 +22,4 @@ public class CommonRpcEncoderHandler extends MessageToByteEncoder<Object> {
 	    ctx.write(byteBufferWrapper.getBuffer());
 	}
 	
-	@Override
-	public void close(ChannelHandlerContext ctx, ChannelPromise promise)
-			throws Exception {
-		// TODO Auto-generated method stub
-		super.close(ctx, promise);
-		CommonRpcTcpClientFactory.getInstance().removeRpcClient(ctx.channel().remoteAddress().toString());
-	}
 }
